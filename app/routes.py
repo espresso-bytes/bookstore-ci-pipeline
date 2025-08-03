75ce6eccfb1b52968b1dbc5b0f1356cc702c5d70
Varsha <vs.varsha333@gmail.com>
2025-08-04 04:00:27 +0530

    ...

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,41 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template, request
+import requests
 
 books_bp = Blueprint('books', __name__)
 
-@books_bp.route('/books', methods=['GET'])
-def get_books():
-    return jsonify([
-        {"id": 1, "title": "The Alchemist", "author": "Paulo Coelho"},
-        {"id": 2, "title": "1984", "author": "George Orwell"},
-        {"id": 3, "title": "To Kill a Mockingbird", "author": "Harper Lee"}
-    ])
+def fetch_books(query="python"):
+    url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        return []
+    
+    data = response.json()
+    books = []
+    for item in data.get('items', []):
+        volume = item.get('volumeInfo', {})
+        sale = item.get('saleInfo', {})
+        price = sale.get('listPrice', {}).get('amount', 'N/A')
+        currency = sale.get('listPrice', {}).get('currencyCode', '')
+        
+        books.append({
+            'title': volume.get('title'),
+            'authors': ", ".join(volume.get('authors', [])),
+            'rating': volume.get('averageRating', 0),
+            'thumbnail': volume.get('imageLinks', {}).get('thumbnail'),
+            'price': f"{price} {currency}" if price != 'N/A' else "Not for Sale"
+        })
+    return books
+
+@books_bp.route('/', methods=['GET'])
+def home():
+    search_query = request.args.get('search', 'python')
+    filter_option = request.args.get('filter', 'none')
+    books = fetch_books(search_query)
+
+    if filter_option == 'high_rating':
+        books.sort(key=lambda x: x.get('rating', 0), reverse=True)
+    elif filter_option == 'low_price':
+        books = [b for b in books if isinstance(b.get('price'), str) and b['price'] != 'Not for Sale']
+        books.sort(key=lambda x: float(x['price'].split()[0]))
+
+    return render_template('books.html', books=books, search_query=search_query, selected_filter=filter_option)
